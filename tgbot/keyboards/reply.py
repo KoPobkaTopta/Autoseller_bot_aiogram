2cf6bc2c9eaf732b86125794d023ac33ca0fa8b7
KoPobkaTopta <96625979+KoPobkaTopta@users.noreply.github.com>
2024-11-26 23:58:32 +0500

    Update reply.py

--- tgbot/keyboards/reply.py
+++ tgbot/keyboards/reply.py
@@ -12,8 +12,8 @@ menu_keyboard = ReplyKeyboardMarkup(
 choose_plan_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Тариф 1 мес. - 150 руб.")],
-        [KeyboardButton(text="Тариф 3 мес. - 450 руб")],
-        [KeyboardButton(text="Тариф 6 мес. - 900 руб")],
+        [KeyboardButton(text="Тариф 3 мес. - 450 руб.")],
+        [KeyboardButton(text="Тариф 6 мес. - 900 руб.")],
     ],
     resize_keyboard=True,
     one_time_keyboard=True,
