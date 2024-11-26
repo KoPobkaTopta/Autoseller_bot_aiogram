5954512045853c0153e8bfbc67d4b336d7b75ed2
KoPobkaTopta <96625979+KoPobkaTopta@users.noreply.github.com>
2024-11-26 20:31:18 +0500

    Update reply.py

--- tgbot/keyboards/reply.py
+++ tgbot/keyboards/reply.py
@@ -4,16 +4,15 @@ menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Оплатить"), KeyboardButton(text="Мои настройки")],
         [KeyboardButton(text="Профиль"), KeyboardButton(text="Поддержка")],
-        [KeyboardButton(text="🔥 АКЦИЯ!!! 🔥 ⏱ Пробный период на 3 дня")],
     ],
     resize_keyboard=True,
 )
 
 choose_plan_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Тариф 3 мес. - 600 руб.")],
-        [KeyboardButton(text="Тариф 6 мес. - 900 руб.(скидка 50% 🔥)")],
-        [KeyboardButton(text="Тариф 1 год - 1350 руб.(скидка 70% 🔥)")],
+        [KeyboardButton(text="Тариф 1 мес. - 150 руб.")],
+        [KeyboardButton(text="Тариф 3 мес. - 450 руб")],
+        [KeyboardButton(text="Тариф 6 год - 900 руб")],
     ],
     resize_keyboard=True,
     one_time_keyboard=True,
